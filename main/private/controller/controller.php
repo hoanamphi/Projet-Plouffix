e91018fa9998405de33919a5961d1c799068d5a1
jonathanchabaud <jonathan.chabaud@etu.univ-nantes.fr>
2017-11-16 20:56:49 +0100

    Starting Coding

--- main/private/controller/controller.php
+++ main/private/controller/controller.php
@@ -1,20 +1,28 @@
 <?php
-function formatVerification(){
-    if(isset($_GET['num'])) {
-        $num = $_GET['num'];
-        $pattern = array();
-        $pattern[0] = '/\d*?\./';
-        $replacement = array();
-        $replacement[0] = '';
-        $num = preg_replace($pattern, $replacement, $num);
-        $num = shell_exec('look ' . $num . ' list.txt');
-        echo $num;
-
-        return true;
-        // valider le format ????
+function formatVerification($num){
+    if(isset($num)) {
+        if(strlen($num) >= 6){
+            if(preg_match("[0-9]*(.|,)*[0-9]*", $num)){
+                $pattern = array();
+                $pattern[0] = '/\d*?\./';
+                $replacement = array();
+                $replacement[0] = '';
+                $num = preg_replace($pattern, $replacement, $num);
+                return $num;
+                //$num = shell_exec('look ' . $num . ' list.txt');
+            }
+            else{
+                return null;
+                // pas le droit d'entrer de caractères
+            }
+        }
+        else{
+            return null;
+            // dire de rajouter des décimales
+        }
     }
     else{
-        return false;
+        return null;
         // envoyer message d'erreur ???
     }
 }
