d470015baf3706fd1fb1734ea8ca5936c7adaf1c
Ptipiak <rahael_b-p@hotmail.fr>
2017-11-16 21:17:28 +0100

    first lines of code

--- main/private/controller/controller.php
+++ main/private/controller/controller.php
@@ -1,20 +1,36 @@
 <?php
-function formatVerification(){
-    if(isset($_GET['num'])) {
-        $num = $_GET['num'];
-        $pattern = array();
-        $pattern[0] = '/\d*?\./';
-        $replacement = array();
-        $replacement[0] = '';
-        $num = preg_replace($pattern, $replacement, $num);
-        $num = shell_exec('look ' . $num . ' list.txt');
-        echo $num;
+//Formalise la string ressu et renvoit une string formalisé (contient uniquement des int, sans virgule) ou renvoit un message d'erreur sur la view
+  function inputFormatisation(){
+      if(isset($_GET['num'])) {
+          $num = $_GET['num'];
+          $pattern = array();
+          $pattern[0] = '/\d*?\./';
+          $replacement = array();
+          $replacement[0] = '';
+          $num = preg_replace($pattern, $replacement, $num);
+          $num = shell_exec('look ' . $num . ' list.txt');
+          return $num;
+      } else{
+          return null;
+        }
+      }
 
-        return true;
-        // valider le format ????
+    //recherche le numéro dans le txt donné par rechercheRepertoire
+    function rechercheNum() {
+
     }
-    else{
-        return false;
-        // envoyer message d'erreur ???
+
+    //recherche le reperdoire dans le quel se trouve le txt contenant le numéro donné par inputFormatisation
+    function rechercheRepertoire() {
+
+    }
+
+    // retourne le résultat trouvé par rechercheNum, sinon soit ne fait rien, soit renvoit un message d'erreur si la chaîne est vraiment non formlisable
+    function returningList() {
+
+      if (inputFormatisation() != null) {
+
+      }
+
     }
 }
---
+++ main/private/view/lookup.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8" lang="fr">
+</head>
+
+<body>
+  <t1>Inverter de Plouffe</t1>
+    <form action="../controller/controller.php" method="get">
+      <input type="number" name="num">
+      <input type="submit">
+    </form>
+    <?php
+      function affichageNum() {
+
+      }
+
+      function affichageErrorMsg() {
+        
+      }
+     ?>
+</body>
+
+</html>
